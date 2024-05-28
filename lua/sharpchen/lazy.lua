685a98bf1e3737d8a1c6492618b5fddbbf03a7df
sharpchen <rui.chen.sharp@gmail.com>
2024-05-28 21:27:11 +0800

    commit

--- lua/sharpchen/lazy.lua
+++ lua/sharpchen/lazy.lua
@@ -27,8 +27,17 @@ local plugins = {
     'tpope/vim-fugitive',
     { 'VonHeikemen/lsp-zero.nvim',           branch = 'v3.x' },
     'neovim/nvim-lspconfig',
+
     'hrsh7th/cmp-nvim-lsp',
+
+    'hrsh7th/cmp-buffer',
+
+    'hrsh7th/cmp-path',
+
+    'hrsh7th/cmp-cmdline',
+
     'hrsh7th/nvim-cmp',
+
     {
         'L3MON4D3/LuaSnip',
         dependencies = { "rafamadriz/friendly-snippets" },
