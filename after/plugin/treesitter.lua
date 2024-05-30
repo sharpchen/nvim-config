6ff143ff9561ae09461eb59091105a5dcf9506d6
sharpchen <rui.chen.sharp@gmail.com>
2024-05-30 22:14:49 +0800

    treesitter: ensure toml, yaml

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup({
-    ensure_installed = { "c", 'cpp', 'css', 'scss', 'asm', 'bash', 'diff', "lua", 'luap', 'luadoc', "vim", "vimdoc", "rust", "c_sharp", "typescript", "javascript", 'jsdoc', "html", 'json', 'xml', 'java', 'kotlin', 'haskell', 'sql', 'python', 'csv', 'vue', 'dockerfile', 'gitignore', 'gitcommit', 'gitattributes', 'git_config', 'go', 'query' },
+    ensure_installed = { "c", 'cpp', 'css', 'scss', 'asm', 'bash', 'diff', "lua", 'luap', 'luadoc', "vim", "vimdoc", "rust", "c_sharp", "typescript", "javascript", 'jsdoc', "html", 'json', 'xml', 'java', 'kotlin', 'haskell', 'sql', 'python', 'csv', 'vue', 'dockerfile', 'gitignore', 'gitcommit', 'gitattributes', 'git_config', 'go', 'query', 'toml', 'yaml', },
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
