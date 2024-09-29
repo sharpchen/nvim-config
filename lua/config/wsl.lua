9b947a1e287555e763c8c73bec4b3985243fa18d
sharpchen <rui.chen.sharp@gmail.com>
2024-09-29 15:43:18 +0800

    grr

--- lua/config/wsl.lua
+++ lua/config/wsl.lua
@@ -1,9 +1,13 @@
 local function set_windows_clipboard()
+  local bin = vim.fn.expand('~/.local/bin')
+  if vim.fn.isdirectory(bin) == 0 then
+    os.execute('mkdir -p ' .. bin)
+  end
   local win32yank_path = vim.fn.expand('~/.local/bin/win32yank.exe')
   if vim.uv.fs_stat(win32yank_path) == nil then
     local install_script_path = '/tmp/install_win32yank.sh'
     local Task = require('plenary.job')
-    local fetch = Task:new({
+    local download_script = Task:new({
       command = 'wget',
       args = {
         '-nv',
@@ -15,13 +19,13 @@ local function set_windows_clipboard()
         vim.notify(vim.fn.escape(data, '"') .. '"', vim.log.levels.TRACE)
       end,
       on_start = function()
-        vim.notify('Downloading win32yank...', vim.log.levels.INFO)
+        vim.notify('Downloading script...', vim.log.levels.INFO)
       end,
       on_exit = function()
-        vim.notify('win32yank downloaded.', vim.log.levels.INFO)
+        vim.notify('script downloaded.', vim.log.levels.INFO)
       end,
     })
-    local chmod = Task:new({
+    local chmod_script = Task:new({
       command = 'chmod',
       args = { '+x', install_script_path },
       on_stderr = function(_, val)
@@ -31,19 +35,33 @@ local function set_windows_clipboard()
         vim.notify(vim.fn.escape(val, '"'), vim.log.levels.INFO)
       end,
     })
-    local execute = Task:new({
+    local download_winyank32 = Task:new({
       command = 'sh',
       args = { install_script_path },
       on_start = function()
         vim.notify('Installing win32yank...', vim.log.levels.INFO)
       end,
+      on_stderr = function(_, val)
+        vim.notify(vim.fn.escape(val, '"'), vim.log.levels.ERROR)
+      end,
       on_exit = function()
         vim.notify('win32yank installed.', vim.log.levels.INFO)
       end,
     })
-    chmod:and_then(execute)
-    fetch:and_then(chmod)
-    fetch:start()
+    local chmod_exe = Task:new({
+      command = 'chmod',
+      args = { '+x', win32yank_path },
+      on_stderr = function(_, val)
+        vim.notify(vim.fn.escape(val, '"'), vim.log.levels.ERROR)
+      end,
+      on_stdout = function(_, val)
+        vim.notify(vim.fn.escape(val, '"'), vim.log.levels.INFO)
+      end,
+    })
+    download_winyank32:and_then(chmod_exe)
+    chmod_script:and_then(download_winyank32)
+    download_script:and_then(chmod_script)
+    download_script:start()
   end
   vim.g.clipboard = {
     name = 'win32yank-wsl',
